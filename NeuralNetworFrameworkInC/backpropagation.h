1618017e4975e6bfe81b48b3db4a929df595c26e
Rodrigo Morales <andresmorales.ro@gmail.com>
2022-05-29 12:28:46 +0200

    Added header files for basic NN elements

---
+++ NeuralNetworFrameworkInC/backpropagation.h
@@ -0,0 +1,37 @@
+#ifndef BACKPROP_H
+#define BACKPROP_H
+
+#include "layer.h"
+
+#define SUCCESS_INIT 0
+#define ERR_DINIT 1
+
+#define SUCCESS_IDNIT 0
+#define ERR_DINIT 1
+
+#define SUCCESS_INIT_WEIGHTS 0
+#define ERR_INIT_WEIGHTS 1
+
+#define SUCCESS_UPDATE_WEIGHTS 0
+
+#define SUCCESS_CREATE_ARCHITECTURE 0
+#define ERR_CREATE_ARCHITECTURE 0
+
+int init(void);
+int dinit(void);
+
+int create_architecture(void);
+int initialize_wights(void);
+void input_data(int i);
+void train(void);
+void forward_prop(void);
+void caculate_cost(int i);
+void back_prop(void);
+void updated_wights(void);
+void get_inputs(void);
+void get_outputs(void);
+void test_nn(void);
+
+int initialize_dummy_weights(void);
+
+#endif
---
+++ NeuralNetworFrameworkInC/layer.h
@@ -0,0 +1,7 @@
+#include <neuron.h>
+
+typedef struct layer_t
+{
+  int num_neurons;
+  struct neuron_t *neuron;
+} layer;
